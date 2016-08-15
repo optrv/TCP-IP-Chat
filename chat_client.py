a4766d0bdb6d1964c8c2482cb73d39c1018f5ae2
Oleksandr Petrov <petr.ov@icloud.com>
2016-08-15 10:17:07 +0300

    TCP-IP-Chat 0.0.9
    The early version without args/config-parser.

---
+++ chat_client.py
@@ -0,0 +1,45 @@
+import socket
+
+def input_urname(fn):
+    def welcome(*urname):
+        urname = input('Please, input your name: ')
+        if urname != '':
+            fn(urname)
+        fn(*urname)
+    return welcome
+
+@input_urname
+def conn(urname = 'Anonymous'):
+    print('Hello, ' + urname.upper() + '!')
+    print('Welcome to Chat!')
+    ssocket = socket.socket()
+    # For debug
+    try:
+        ssocket.connect(('', 8080))
+    except OSError:
+        ssocket.close()
+        ssocket.connect(('', 9090))
+    # Send your name
+    ssocket.send(urname.encode('utf-8'))
+    # Receive its name
+    itsname = ssocket.recv(1024)
+    itsname = itsname.decode("utf-8")
+    print("You start the chat with " + itsname.upper() + "!")
+
+    while True:
+        msg = input("YOU: ")
+        ssocket.send(msg.encode('utf-8'))
+        print("*Waiting for %s...*" % itsname.upper())
+        data = ssocket.recv(16000)
+        data = data.decode("utf-8")
+        print(itsname.upper() + ": " + data)
+        if not data:
+            break
+
+    ssocket.close()
+
+def main():
+    conn()
+
+if __name__ == '__main__':
+    main()
